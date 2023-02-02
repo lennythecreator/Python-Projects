6ed6c8247e77d9e987b0de6bba9495528224f639
Lenny Uwaeme <48912140+lennythecreator@users.noreply.github.com>
2023-02-02 10:23:25 -0500

    Add files via upload
    This is a Script that lets you send Emails from python it is also able to be used in the terminal.

---
+++ Email.py
@@ -0,0 +1,24 @@
+
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = "lenyeuwaeme@gmail.com"
+email_password = 'dwjvdpzbeojyeess'
+email_receiver = input("Send to: ")
+
+subject = input("Subject: ")
+body = input("Enter message: ")
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To']=  email_receiver
+em['subject']= subject
+em.set_content(body)
+
+context = ssl._create_unverified_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+    smtp.login(email_sender,email_password)
+    smtp.sendmail(email_sender,email_receiver, em.as_string())
+print("e-mail sent!")
